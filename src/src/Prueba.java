450c02d1af5e773345580e350442b8d1e48a63fe
Briyith sanchez <briyithd.sancheza@uqvirtual.edu.co>
2026-02-12 15:42:32 -0500

    hola Jhonatan

--- src/src/Prueba.java
+++ src/src/Prueba.java
@@ -2,5 +2,8 @@ public class Prueba {
     public static void main(String[] args) {
         System.out.print("hola");
         System.out.print("\nhola Briyith, ahora necesitamos es que interactue Jhonatan");
+        System.out.println("hola Nicolás");
+        System.out.println("¿cómo está?");
+        System.out.println("Hola Jhonatan");
     }
 }
